11e930747203fdf6a709a9151f315e3354ca3c5b
shreyas-085 <shreyask085@gmail.com>
2020-07-10 13:30:29 +0530

    "Feed fragement created"

--- app/src/main/java/com/example/clik/Fragements/FeedFragment.java
+++ app/src/main/java/com/example/clik/Fragements/FeedFragment.java
@@ -1,19 +1,39 @@
 package com.example.clik.Fragements;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.clik.Adapter.PostAdapter;
+import com.example.clik.Feed.AddPostActivity;
+import com.example.clik.Model.Post;
 import com.example.clik.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FeedFragment extends Fragment {
 
     private FloatingActionButton fab;
+    private RecyclerView recyclerViewPosts;
+    private PostAdapter postAdapter;
+    private List<Post> postList;
+
+//    private List<String> friendsList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,10 +46,46 @@ public class FeedFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(getContext(), AddPostActivity.class));
             }
         });
 
+        recyclerViewPosts = v.findViewById(R.id.recycler_view);
+        recyclerViewPosts.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setStackFromEnd(true);
+        linearLayoutManager.setReverseLayout(true);
+        recyclerViewPosts.setLayoutManager(linearLayoutManager);
+
+        createPostList();
+
         return v;
+    }
+
+    private void createPostList() {
+        final ProgressDialog pd = new ProgressDialog(getContext());
+        pd.setMessage("Please Wait");
+        pd.show();
+
+        FirebaseDatabase.getInstance().getReference().child("posts").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList = new ArrayList<>();
+                for(DataSnapshot snapshot1 : snapshot.getChildren()){
+                    Post post = snapshot1.getValue(Post.class);
+                    postList.add(post);
+                }
+
+                postAdapter = new PostAdapter(getContext(), postList);
+                recyclerViewPosts.setAdapter(postAdapter);
+                pd.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+            }
+        });
     }
 }
