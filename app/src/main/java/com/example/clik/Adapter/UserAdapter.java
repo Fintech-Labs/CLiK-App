55a77b8fb62765aa83e8bd3af1a1235574c63d97
ayushvatsal <vatsalayush31@gmail.com>
2020-07-15 12:37:45 +0530

    search and follow options added

---
+++ app/src/main/java/com/example/clik/Adapter/UserAdapter.java
@@ -0,0 +1,140 @@
+package com.example.clik.Adapter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.clik.Fragements.ProfileFragment;
+import com.example.clik.Model.User;
+import com.example.clik.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+
+    private Context context;
+    private List<User> users;
+
+    private FirebaseUser firebaseUser;
+
+
+    public UserAdapter(Context context, List<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_layout,parent,false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        final User user = users.get(position);
+
+        holder.btnfollow.setVisibility(View.VISIBLE);
+
+        holder.fullname.setText(user.getName());
+        holder.bio.setText(user.getBio());
+
+        Picasso.get().load(user.getProfileUri()).into(holder.profile_pic);
+
+        isFollowing(user.getuId(),holder.btnfollow);
+
+        if(user.getuId().equals(firebaseUser.getUid())){
+            holder.btnfollow.setVisibility(View.GONE);
+        }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = context.getSharedPreferences("PREFS", Context.MODE_PRIVATE).edit();
+                editor.putString("profileid",user.getuId());
+                editor.apply();
+
+                ((FragmentActivity)context).getSupportFragmentManager().beginTransaction().replace(R.id.fragement_container,new ProfileFragment()).commit();
+            }
+        });
+
+        holder.btnfollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(holder.btnfollow.getText().toString().equals("follow")){
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid())
+                            .child("following").child(user.getuId()).setValue(true);
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(user.getuId())
+                            .child("followers").child(firebaseUser.getUid()).setValue(true);
+                }else{
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid())
+                            .child("following").child(user.getuId()).removeValue();
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(user.getuId())
+                            .child("folllowers").child(firebaseUser.getUid()).removeValue();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return users.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView bio;
+        public TextView fullname;
+        public ImageView profile_pic;
+        public Button btnfollow;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bio = itemView.findViewById(R.id.bio);
+            fullname = itemView.findViewById(R.id.name);
+            profile_pic = itemView.findViewById(R.id.profile_photo);
+            btnfollow = itemView.findViewById(R.id.btn_follow);
+        }
+    }
+        private void isFollowing(final String userid, final Button button){
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid()).child("following");
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.child(userid).exists()){
+                      button.setText("following");
+                    }else{
+                        button.setText("follow");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+
+    }
+}
--- app/src/main/java/com/example/clik/Fragements/NotiFragment.java
+++ app/src/main/java/com/example/clik/Fragements/NotiFragment.java
@@ -1,21 +1,120 @@
 package com.example.clik.Fragements;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.example.clik.Adapter.UserAdapter;
+import com.example.clik.Model.User;
 import com.example.clik.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotiFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
+    private List<User> users;
+    EditText search_bar;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_noti, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_noti, container, false);
+
+        recyclerView= view.findViewById(R.id.recyclerView1);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        search_bar = view.findViewById(R.id.search_bar);
+
+        users = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext(),users);
+
+        recyclerView.setAdapter(userAdapter);
+
+        readUsers();
+        search_bar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                searchUsers(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        return view;
+
+    }
+
+    private void searchUsers(String s){
+        Query query = FirebaseDatabase.getInstance().getReference("users").orderByChild("Name")
+                .startAt(s)
+                .endAt(s+"\uf8ff");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for(DataSnapshot snapshot2 :snapshot.getChildren()){
+                    User user = snapshot2.getValue(User.class);
+                    users.add(user);
+                }
+                userAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void readUsers(){
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(search_bar.getText().toString().equals("")){
+                    users.clear();
+                    for(DataSnapshot snapshot3 :snapshot.getChildren()){
+                        User user = snapshot3.getValue(User.class);
+                        users.add(user);
+                    }
+                    userAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
